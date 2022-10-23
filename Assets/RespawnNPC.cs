c129f5d1b534ef0a7eff177b770feafb7b47c167
zaelanirizal02 <zaelanirizal02@gmail.com>
2022-10-23 22:15:54 +0700

    respawnNPC

---
+++ Assets/RespawnNPC.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnNPC : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnPoint;
+
+    void OnTriggerEnter(Collider other) 
+    {
+        player.transform.position = respawnPoint.transform.position;
+    }
+}
